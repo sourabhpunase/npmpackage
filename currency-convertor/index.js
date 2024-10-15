0a6138f091d319d5e66b6979776b63e698257bce
57113SP <sourabh.punase@merai.co>
2024-10-15 17:16:25 +0530

    added currency package

---
+++ currency-convertor/index.js
@@ -0,0 +1,25 @@
+import Freecurrencyapi from '@everapi/freecurrencyapi-js';
+
+const freecurrencyapi = new Freecurrencyapi('fca_live_Bjl83c8Cm4QmmpE9TKpyscCWdaChQKFFgVMluSwo');
+
+
+export async function convertCurrency (fromCurrency,toCurrency,units){
+const res= await freecurrencyapi.latest({
+    base_currency:fromCurrency,
+    currencies:toCurrency
+});
+const multiplier=res.data[toCurrency];
+return multiplier*units;
+
+
+}
+
+
+
+
+// freecurrencyapi.latest({
+//     base_currency: 'USD',
+//     currencies: 'EUR'
+// }).then(response => {
+//     console.log(response);
+// });
